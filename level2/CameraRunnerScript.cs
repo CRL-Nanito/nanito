9d93992f3102af8da8573393cd95b9b9c9a168a7
CRL-Nanito <mxctobyxd@gmail.com>
2015-02-13 16:01:24 -0400

    level 2
    hidrofobia

---
+++ level2/CameraRunnerScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRunnerScript : MonoBehaviour {
+
+	public Transform player;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (0, player.position.y + 3, -10);
+	}
+}

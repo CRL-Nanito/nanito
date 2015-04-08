69358359bd1beb0286d102b671f0dc416cbad70b
CRL-Nanito <mxctobyxd@gmail.com>
2015-04-08 16:38:59 -0400

    Test1
    Update level 1

---
+++ Nanito1.3.2/Nanito1.3.2/Assets/Scripts/FfScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FfScript : MonoBehaviour {
+
+	public int ffNumber = 1;
+	
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+}
---
+++ Nanito1.3.2/Nanito1.3.2/Assets/Scripts/ShieldScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldScript : MonoBehaviour {
+
+	public int shieldNumber = 1;
+	
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+}

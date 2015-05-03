c959f85c4923bce05c492f3af3d18cca2caaa81c
CRL-Nanito <mxctobyxd@gmail.com>
2015-05-03 04:28:17 -0400

    GunPart1

--- Nanito1.3.2/Nanito1.3.2/Assets/Gun.cs
+++ Nanito1.3.2/Nanito1.3.2/Assets/Gun.cs
@@ -1,11 +1,11 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Gun : MonoBehaviour {
 
 	public float fireRate = 0;
 	public float Damage = 10;
-	public LayerMask notToHit;
+	public LayerMask ToHit;
 
 	float timeToFire = 0;
 	Transform firePoint;
@@ -22,20 +22,29 @@ public class Gun : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Shoot ();
 		if (fireRate == 0) {
 			if(Input.GetButtonDown("Fire1")){
 				Shoot();
 
+			}
 		}
-	}
 		else {
 			if(Input.GetButtonDown("Fire1") && Time.time > timeToFire){
 				timeToFire = Time.time + 1/fireRate;
 				Shoot();
 			}
-}
-}
+		}
+	}
 	void Shoot(){
 		Debug.Log ("Test");
+		Vector2 mousePosition = new Vector2 (1f,1f);
+		Vector2 firePointPosition = new Vector2 (firePoint.position.x, firePoint.position.y);
+		RaycastHit2D hit = Physics2D.Raycast (firePointPosition, mousePosition - firePointPosition,1, ToHit);
+		Debug.DrawLine (firePointPosition,mousePosition-firePointPosition,Color.cyan);
+		if (hit.collider != null) {
+			Debug.DrawLine(firePointPosition,hit.point,Color.red);
+			Debug.Log ("We hit "+ hit.collider.name + " and did " + Damage + " damage.");
+		}
 	}
 }
